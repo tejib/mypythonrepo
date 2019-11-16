d00bcd7d83b1af7d7385198db69cc11ae9be44c1
Tej <me.tonyb@gmail.com>
2019-11-16 19:27:54 +0530

    cleaned up code

--- s3listobj.py
+++ s3listobj.py
@@ -2,11 +2,8 @@ import boto3
 s3 = boto3.resource('s3')
 my_bucket = s3.Bucket('sample-bucket')
 
-#print(my_bucket)
-
 for my_bucket_object in my_bucket.objects.all():  # Iterate thru all the objects
     print(my_bucket_object.key)
 
 for my_bucket_filter in my_bucket.objects.filter(Prefix='Code Refer/bmw'):  #Iterate thru a specific folder in S3
     print(my_bucket_filter.key)
-
