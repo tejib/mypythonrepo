697345923d9868b1b4f6b68714e26dc679569d28
Tej <me.tonyb@gmail.com>
2019-12-25 09:52:01 +0530

    Serverless first try

---
+++ 02-notifon/notifier/handler.py
@@ -0,0 +1,10 @@
+import json
+
+
+def hello(event, context):
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
